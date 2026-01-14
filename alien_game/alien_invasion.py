2020c38c43783b6fdb79397e07e4ce1448f4af5d
thatOneGuy <josiah.cruship@gmail.com>
2026-01-14 12:45:01 -0600

    uploding homwork

--- alien_game/alien_invasion.py
+++ alien_game/alien_invasion.py
@@ -2,35 +2,29 @@ import sys
 
 import pygame
 
+from settings import Settings
+
 class AlienInvasion:
-    """
-    Docstring for AlienInvasion
-    """
+    """overall class to manage game assets and behavior"""
     def __init__(self):
-        """
-        Docstring for __init__
-        
-        :param self: Description
-        """
+        """Initalize the game, and create the game res"""
         pygame.init()
         self.clock = pygame.time.Clock()
+        self.settings = Settings()
 
-        self.screen = pygame.display.set_mode((1200, 800))
+        self.screen = pygame.display.set_mode(
+            (self.settings.screen_width, self.settings.screen_height))
         pygame.display.set_caption("Alien Invasion")
 
-        self.bg_color = (160, 44, 43)
+        self.bg_color = (40, 75, 80)
 
     def run_game(self):
-        """
-        Docstring for run_game
-        
-        :param self: Description
-        """
+        """start the main loop for the game"""
         while True:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     sys.exit()
-            self.screen.fill(self.bg_color)
+            self.screen.fill(self.settings.bg_color)
 
             pygame.display.flip()
             self.clock.tick(60)
---
+++ alien_game/settings.py
@@ -0,0 +1,8 @@
+class Settings:
+    """A class for settings. set = settings"""
+    def __init__(self):
+        """intialize the set"""
+        #screen set
+        self.screen_width = 1440
+        self.screen_height = 845
+        self.bg_color = (40, 75, 80)
