c409dbeb498e8079ca173d3b539cc289e0200c1a
thatOneGuy <josiah.cruship@gmail.com>
2026-01-18 13:53:16 -0600

    i finalli got thew ship thew right size

--- alien_game/ship.py
+++ alien_game/ship.py
@@ -9,11 +9,15 @@ class Ship:
       self.screen_rect = ai_game.screen.get_rect()
 
       self.image = pygame.image.load('images/ship.bmp')
+      width = self.image.get_rect().width
+      height = self.image.get_rect().height
       self.rect = self.image.get_rect()
+
+      self.image = pygame.transform.scale(self.image, (width/5, height/5))
 
       self.rect.midbottom = self.screen_rect.midbottom
 
    def blitme(self):
-    """draw the ship"""
-    self.screen.blit(self.image, self.rect)
+       """draw the ship"""
+       self.screen.blit(self.image, self.rect)
 
