a4cdeb4041f40ef02405a61e4a5e2f6c1773cbd3
thatOneGuy <josiah.cruship@gmail.com>
2026-01-22 18:53:57 -0600

    i

--- alien_game/alien_invasion.py
+++ alien_game/alien_invasion.py
@@ -21,6 +21,7 @@ class AlienInvasion:
         self.settings.screen_width = self.screen.get_rect().width
         self.settings.screen_height = self.screen.get_rect().height
         pygame.display.set_caption("Alien Invasion by: Thqt0ne6nh")
+        
 
         # Create an instance to store game_stats
         self.stats = GameStats(self)
--- alien_game/ship.py
+++ alien_game/ship.py
@@ -13,7 +13,6 @@ class Ship:
       self.image = pygame.image.load('images/ship.bmp')
       self.rect = self.image.get_rect()
 
-      
       # start each new ship at the bottem center of the screen
       self.rect.midbottom = self.screen_rect.midbottom
 
