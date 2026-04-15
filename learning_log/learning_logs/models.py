3cee5afc6cb42dc697f262e546f140ec4dae880e
thatOneGuy <josiah.cruship@gmail.com>
2026-04-15 09:57:57 -0500

    uploding

--- learning_log/learning_logs/models.py
+++ learning_log/learning_logs/models.py
@@ -1,4 +1,5 @@
-from django.db import models
+from django.db import models\
+    
 
 class Topic(models.Model):
     """A topic the user is learning about."""
@@ -8,3 +9,15 @@ class Topic(models.Model):
     def __str__(self):
         """Return a string repersentation of a model."""
         return self.text
+class Entry(models.Model):
+    """Somthing spacific learned about a topic"""
+    topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
+    text = models.TextField()
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name_plural = 'entries'
+    
+    def __str__(self):
+        """return a simple string repersenting the entry"""
+        return f"{self.text[:50]}..."
