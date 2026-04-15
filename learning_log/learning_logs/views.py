3f486d89f54cb3d1ea7795127288448ca89a1ae9
thatOneGuy <josiah.cruship@gmail.com>
2026-04-15 11:42:11 -0500

    made index.html

--- learning_log/learning_logs/views.py
+++ learning_log/learning_logs/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
 
-# Create your views here.
+def index(request):
+    """the home page for learning_log"""
+    return render(request, 'learning_logs/index.html')
