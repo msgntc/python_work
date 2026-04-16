8ea4752d763f8daa8ac1fdf47aaf0d0c6c94b55d
thatOneGuy <josiah.cruship@gmail.com>
2026-04-16 10:46:31 -0500

    i did stuff

--- learning_log/learning_logs/views.py
+++ learning_log/learning_logs/views.py
@@ -6,8 +6,9 @@ def index(request):
     """the home page for learning_log"""
     return render(request, 'learning_logs/index.html')
 
-def topic(request):
-    """show all topics."""
-    topics = Topic.objects.order_by('date_added')
-    context = {'topics': topics}
+def topic(request, topic_id):
+    """show a single topic and all its ."""
+    topic = Topic.objects.get(id=topic_id)
+    entries = Topic.entry_set.order_by('-date_added')
+    context = {'topic': topic, 'entries': entries}
     return render(request, 'learning_logs/topics.html', context)
