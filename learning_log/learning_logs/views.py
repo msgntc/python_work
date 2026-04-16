4d19e04cae8b2ede883b7af250ef5a10b9b921f6
thatOneGuy <josiah.cruship@gmail.com>
2026-04-16 11:55:12 -0500

    found anouther extra s.

--- learning_log/learning_logs/views.py
+++ learning_log/learning_logs/views.py
@@ -17,4 +17,4 @@ def topic (request, topic_id):
     topic = Topic.objects.get(id=topic_id)
     entries = topic.entry_set.order_by('-date_added')
     context = {'topic': topic, 'entries': entries}
-    return render(request, 'learning_logs/topics.html', context)
+    return render(request, 'learning_logs/topic.html', context)
