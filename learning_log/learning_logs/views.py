dd52322e9db1c4f48baf64457ecd9d790ca96436
thatOneGuy <josiah.cruship@gmail.com>
2026-04-16 17:20:00 -0500

    added forms.py and new_topic.html

--- learning_log/learning_logs/views.py
+++ learning_log/learning_logs/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 from .models import Topic
+from .forms import TopicForm
 
 def index(request):
     """the home page for learning_log"""
@@ -18,3 +19,18 @@ def topic (request, topic_id):
     entries = topic.entry_set.order_by('-date_added')
     context = {'topic': topic, 'entries': entries}
     return render(request, 'learning_logs/topic.html', context)
+
+def new_topic(request):
+    """add a new topic."""
+    if request.method != 'POST':
+        # no data submited blanck form
+        form = TopicForm()
+    else:
+        # post data submited; prosess data.
+        form = TopicForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('learning_logs:topics')
+    # desplay a blank or invalid form.
+    context = {'form': form}
+    return render(request, 'learning_logs/new_topic.html', context)
