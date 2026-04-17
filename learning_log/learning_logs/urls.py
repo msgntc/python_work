30cf5494f6f2fc22738e5a22df7d8580721516e3
thatOneGuy <josiah.cruship@gmail.com>
2026-04-16 19:08:43 -0500

    added the abilaty for users to edit entry

--- learning_log/learning_logs/urls.py
+++ learning_log/learning_logs/urls.py
@@ -16,5 +16,6 @@ urlpatterns = [
     path('new_topic/', views.new_topic, name='new_topic'),
     # page for adding a new entry.
     path('new_enty/<int:topic_id>/', views.new_entry, name='new_entry'),
-    
+    #page for editing an entry
+    path('edit_entry/<int:entry_id>/', views.edit_entry, name='edit_entry'),
 ]
